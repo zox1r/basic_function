baac32412c3fa61eee5031525de4d12cf7ada794
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:21:37 +0200

    bin/basic_function08.dart

--- bin/basic_function08.dart
+++ bin/basic_function08.dart
@@ -1,1 +1,9 @@
 // Create a function called `isWeekend` that returns a `bool`. The function should check if today is a weekend day (Saturday or Sunday) and return `true` if it is, and `false` otherwise.
+bool isWeekend(){
+    String x = "tuesday";
+    String y = "Sinday";
+    return x == y;
+}
+void main(){
+    print(isWeekend());
+}
