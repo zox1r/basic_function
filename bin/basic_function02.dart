ebf84c66294ac6b2aff3b2f218e8528d22a52f53
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 08:57:19 +0200

    bin/basic_function02.dart

--- bin/basic_function02.dart
+++ bin/basic_function02.dart
@@ -1,1 +1,9 @@
 // Create a function called `getNumber` that returns an `int`. The function should return the number 10.
+int getNumber(){
+    return 10;
+}
+
+void main(){
+    print(getNumber());
+
+}
