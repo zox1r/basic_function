db1abfcda595c2a6aa8641d4847dafc72df921bf
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:00:55 +0200

    bin/basic_function03.dart

--- bin/basic_function03.dart
+++ bin/basic_function03.dart
@@ -1,1 +1,7 @@
 // Create a function called `getPiValue` that returns a `double`. The function should return the value of Pi (3.14159).
+double getPiValue(){
+    return 3.14159;
+}
+void main(){
+    print(getPiValue());
+}
