7d9a5b4f3bae6f291880e052f1673a65f160ab36
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:12:56 +0200

    bin/basic_function09.dart

--- bin/basic_function09.dart
+++ bin/basic_function09.dart
@@ -1,1 +1,7 @@
 // Create a function called `getFavoriteColor` that returns a `String`. The function should return your favorite color.
+String getFavoriteColor(){
+    return "black";
+}
+void main(){
+    print(getFavoriteColor());
+}
