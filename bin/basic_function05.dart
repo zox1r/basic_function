479904af960e806c581c7385a469c24a4d05458c
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:07:01 +0200

    bin/basic_function05.dart

--- bin/basic_function05.dart
+++ bin/basic_function05.dart
@@ -1,1 +1,7 @@
 // Create a function called `getFullName` that returns a `String`. The function should return your full name.
+String getFullName(){
+    return "zox1rshox";
+}
+void main(){
+    print(getFullName());
+}
