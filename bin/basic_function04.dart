57fc820e97abd5059830e9eaa757d4b0482ee4ab
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:18:16 +0200

    bin/basic_function04.dart

--- bin/basic_function04.dart
+++ bin/basic_function04.dart
@@ -1,8 +1,9 @@
 // Create a function called `isEven` that returns a `bool`.And create variable inside fucntion called number type of int and give value 5. The function should check if a given number is even and return `true` if it is, and `false` otherwise.
 bool isEven(){
-    int x = 5;
-    double y = 5 % 2;
-    return z ;
+  int x = 5;
+  int y = 5 % 2;
+   
+    return y == 0;
 }
 void main(){
     print(isEven());
