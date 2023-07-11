b19ffd38714bdc38cfe56d4da5b50edc4eae9aee
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 08:58:50 +0200

    bin/basic_function01.dart

--- bin/basic_function01.dart
+++ bin/basic_function01.dart
@@ -1,1 +1,7 @@
 // Create a function called `getGreeting` that returns a `String`. The function should return the string "Hello!".
+String getGreeting(){
+    return "hello";
+}
+void main(){
+    print(getGreeting());
+}
