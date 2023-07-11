6dd486de48f3a67630e0b1487b3f6d0a8dc0ca63
zox1r <qarshiyevzoxir@gmail.com>
2023-07-11 09:10:14 +0200

    bin/basic_function07.dart

--- bin/basic_function07.dart
+++ bin/basic_function07.dart
@@ -1,1 +1,7 @@
 // Create a function called `getTemperature` that returns a `double`. The function should return the current temperature in degrees Celsius.
+double getTemperature(){
+    return 40.1;
+}
+void main(){
+    print(getTemperature());
+}
